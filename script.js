b7a44d15da3f6604f8759e931cffea9581004b7d
handevyanktesh <handevyanktesh99@gmail.com>
2026-02-09 14:53:14 +0530

    Update script.js
    Added task function into js file.

--- script.js
+++ script.js
@@ -8,3 +8,41 @@ const progressBar = document.getElementById("progressBar");
 function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+function renderTasks() {
+  taskList.innerHTML = "";
+  let completed = 0;
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    if (task.done) {
+      li.style.textDecoration = "line-through";
+      completed++;
+    }
+li.innerHTML = `
+      <span>${task.text} ${task.mood}</span>
+      <div>
+        <button class="btn btn-sm btn-success me-1">✓</button>
+        <button class="btn btn-sm btn-danger">✕</button>
+      </div>
+    `;
+
+    li.querySelector(".btn-success").onclick = () => {
+      task.done = !task.done;
+      saveTasks();
+      renderTasks();
+    };
+
+    li.querySelector(".btn-danger").onclick = () => {
+      tasks.splice(index, 1);
+      saveTasks();
+      renderTasks();
+    };
+
+    taskList.appendChild(li);
+  });
+
+  const percent = tasks.length ? (completed / tasks.length) * 100 : 0;
+  progressBar.style.width = percent + "%";
+  progressBar.innerText = Math.round(percent) + "%";
+}
